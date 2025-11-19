7376ef29f79d1e70da35eb14a0c5ab1e114eb0bf
LinweiLi <linweili1998@outlook.com>
2025-11-19 08:33:09 -0600

    [LinweiLi] remove comments

--- src/main/java/com/demo/funchat/util/JwtUtil.java
+++ src/main/java/com/demo/funchat/util/JwtUtil.java
@@ -11,9 +11,8 @@ import java.util.Date;
 public class JwtUtil {
 
     private final String SECRET_KEY = "8e435e4f2a530f331a64948e38cd4e063ddac96447c52011f611b4c8d853ea9e";
-    private final long EXPIRATION_MS = 604800000; // 7天
+    private final long EXPIRATION_MS = 604800000;
 
-    // 生成 token
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
@@ -23,7 +22,6 @@ public class JwtUtil {
                 .compact();
     }
 
-    // 验证 token 并返回用户名
     public String validateToken(String token) {
         return Jwts.parser()
                 .setSigningKey(SECRET_KEY)
